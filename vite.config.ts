81cf305aa541d3c6c9df54b82050e61957e3183f
website1975 <trieuhaminh@gmail.com>
2026-08-18 22:45:41 +0700

    Add path alias resolution in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -12,6 +13,11 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.')
+      }
+    },
     server: {
       host: '0.0.0.0',
       port: 3000,
