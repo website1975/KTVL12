4a749b8904a1d725506eba8fa84db102cdbe85ba
website1975 <trieuhaminh@gmail.com>
2026-08-21 06:35:20 +0700

    Rename EduQuiz to TNMenu in AdminDashboard

--- components/admin/AdminDashboard.tsx
+++ components/admin/AdminDashboard.tsx
@@ -883,7 +883,7 @@ export default function AdminDashboard() {
       <aside className="w-16 lg:w-64 bg-slate-900 text-white flex flex-col shrink-0 transition-all">
         <div className="p-4 lg:p-8 border-b border-white/10 text-center lg:text-left">
           <h2 className="text-xl font-black uppercase tracking-tighter italic">
-            <span className="hidden lg:inline">EduQuiz <span className="text-blue-500">PRO</span></span>
+            <span className="hidden lg:inline">TNMenu <span className="text-blue-500">_U60</span></span>
             <span className="lg:hidden text-blue-500">EQ</span>
           </h2>
         </div>
